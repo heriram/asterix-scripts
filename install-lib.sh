cc0295297c24f2939c29686d8b2efd84b45635e9
thormartin91 <thorma91@gmail.com>
2016-11-08 12:32:25 +0100

    add script to install library

---
+++ install-lib.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# READ VARIABLES
+source asterix.config
+
+echo "----------------------------"
+date
+echo "Installing library"
+
+# Stop Asterix
+/bin/bash stop.sh
+
+echo "Packaging library"
+mvn -f $LIB_DIR/pom.xml clean package -DskipTests
+
+echo "Installing library"
+$MANAGIX install -n $INSTANCE_NAME -d $DATAVERSE -l $LIB_NAME -p $LIB_PATH
+
+# Start Asterix
+/bin/bash start.sh
+
+echo "Library installed!"
+echo "----------------------------"
